b1f46f51c05273312e60f91888f200746f35acb0
banoo Gargari <banoo.gargari@gmail.com>
2021-03-30 21:58:41 -0700

    added the join table

--- app.py
+++ app.py
@@ -83,6 +83,12 @@ def delete_todo(todo_id):
 
 
 # // render_template in order to specify an HTML file to render to the user whenever our users visits this route.
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    lists=TodoList.query.all(), 
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
